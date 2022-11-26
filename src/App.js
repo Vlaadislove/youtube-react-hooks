6a6d9ff0f8f64279c78cd623060b414265a43e03
Vlaadislove <112594694+Vlaadislove@users.noreply.github.com>
2022-11-26 23:33:15 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import TodoList from "./TodoList";
+
+export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoTitle, setTodoTitle] = useState("");
+
+  useEffect(() => {
+    const raw = localStorage.getItem("todos") || [];
+    setTodos(JSON.parse(raw));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (e) => {
+    if (e.key === "Enter") {
+      setTodos([
+        ...todos,
+        { id: Date.now(), title: todoTitle, completed: false }
+      ]);
+      setTodoTitle("");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>Todo app</h1>
+
+      <div className="input-field">
+        <input
+          type="text"
+          value={todoTitle}
+          onChange={(e) => setTodoTitle(e.target.value)}
+          onKeyPress={addTodo}
+        />
+        <label>Todo name</label>
+      </div>
+
+      <TodoList todos={todos} />
+    </div>
+  );
+}
